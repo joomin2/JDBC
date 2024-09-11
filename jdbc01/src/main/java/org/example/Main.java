3eec490d0847dba28278c84534b9206df0e0f2a8
joomin2 <joomin109@naver.com>
2024-09-11 13:56:47 +0900

    test

--- jdbc01/src/main/java/org/example/Main.java
+++ jdbc01/src/main/java/org/example/Main.java
@@ -1,8 +1,38 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    // 1. MySQL JDBC Driver
+    public static void main(String[] args) {
+        //Mysql jDBC Driver Loading
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver not found");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        // 2.cONNECTION
+        String url = "jdbc:mysql://localhost:3306/smdb";
+        String sqlid = "smuser";
+        String sqlpwd = "111111";
 
+        Connection conn = null;
+        try {
+            try {
+                conn = DriverManager.getConnection(url, sqlid, sqlpwd);
+            } catch (SQLException e) {
+                System.out.println("Connection failed");
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+        } finally {
+
+        }
+    }
 }
+
